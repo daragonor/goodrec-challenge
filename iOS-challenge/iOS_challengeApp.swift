72fb64558aa1afa78fdb53fa53768c7ecc1dcf57
Daniel Aragon <daragonor@icloud.com>
2024-07-11 22:50:36 -0500

    Initial Commit

---
+++ iOS-challenge/iOS_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_challengeApp.swift
+//  iOS-challenge
+//
+//  Created by 🐉 on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
